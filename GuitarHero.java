444570609de144f303fe84e548c5fae0782362d8
JJ27 <66833672+JJ27@users.noreply.github.com>
2022-09-14 12:50:40 -0400

    Day 3

--- GuitarHero.java
+++ GuitarHero.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GuitarHero {
     static ArrayList<String> pieces = new ArrayList<String>();
@@ -38,16 +40,39 @@ public class GuitarHero {
             String[] current = pieces.get(i).split("");
             for(int j = 0; j < current.length; j++){
                 if(current[j].equals("o") || current[j].equals("*")){
-                    notes += map.get((i/measures)+""+j);
+                    notes += (i/measures)+""+j;
                 }
             }
         }
         System.out.println(notes);
+        String[] array = Stream.of(guitarnotes)
+                .flatMap(Stream::of)
+                .toArray(String[]::new);
+        ArrayList<String> noteslist = new ArrayList<String>(Arrays.asList(array));
+        noteslist.add("Measure");
+        ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
+        grid.add(noteslist);
+        ArrayList<String> curradd = new ArrayList<String>(Collections.nCopies(60, " "));
+        for(int i = 0; i < notes.length(); i+=2){
+            String curr = notes.substring(i, i+2);
+            int note = (6 * Integer.parseInt(curr.substring(0,1))) + Integer.parseInt(curr.substring(1));
+            curradd.set(note, "O");
+        }
+        grid = transpose(grid);
+        print(grid);
     }
     public static void print(String[][] arr){
         for(int i = 0; i < arr.length; i++){
             for(int j = 0; j < arr[i].length; j++){
                 System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void print(ArrayList<ArrayList<String>> arr){
+        for(int i = 0; i < arr.size(); i++){
+            for(int j = 0; j < arr.get(i).size(); j++){
+                System.out.print(arr.get(i).get(j) + "\t");
             }
             System.out.println();
         }
@@ -62,6 +87,28 @@ public class GuitarHero {
             }
         }
         return transposed;
+    }
+
+    //transpose the rows and columns of a 2D ArrayList
+    public static ArrayList<ArrayList<String>> transpose(ArrayList<ArrayList<String>> orig) {
+        String[][] array = orig.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);
+
+        String[][] transposed = new String[array[0].length][array.length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                transposed[j][i] = array[i][j];
+            }
+        }
+        return arrayToArrayList(transposed);
+    }
+
+    //2D Array to 2D ArrayList
+    public static ArrayList<ArrayList<String>> arrayToArrayList(String[][] array) {
+        ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(new ArrayList<String>(Arrays.asList(array[i])));
+        }
+        return list;
     }
 
     //2D Array to Hashmap
